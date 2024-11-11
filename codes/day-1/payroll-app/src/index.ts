c8cf14670b03446a1975e2f98ec41b4567534e9d
Joydip <joydip.mondal@gmail.com>
2024-11-11 17:16:32 +0530

    TypeScript code uploaded

--- codes/day-1/payroll-app/src/index.ts
+++ codes/day-1/payroll-app/src/index.ts
@@ -1,3 +1,4 @@
+//models
 class Employee {
     id: number;
     name: string;
@@ -18,19 +19,92 @@ class Employee {
     }
 }
 
-const anilEmployee = new Employee(1, 'anil', 1000, 2000, 3000)
-const sunilEmployee = new Employee(2, 'sunil', 1500, 2500, 3500)
-
+//repository
 const employees: Employee[] = []
 
-employees.push(anilEmployee, sunilEmployee)
+//contract
+interface Manager<T> {
+    add(obj: T): boolean;
+    getAll(): Readonly<T[]>;
+    sort(choice: number): Readonly<T[]>;
+}
 
-employees.forEach(
-    function (e) {
-        e.calculateSalary()
-        console.log(`${e.name} has got salary of ${e.totalSalary}`);
+//implementation
+class EmployeeManager implements Manager<Employee> {
+    add(obj: Employee): boolean {
+        try {
+            let found = employees.find(e => e.id === obj.id)
+            if (found)
+                throw new Error('employee with same id already exists..')
+            else {
+                employees.push(obj)
+                return true
+            }
+        } catch (error) {
+            throw error
+        }
     }
-)
+    getAll(): Readonly<Employee[]> {
+        return [...employees]
+    }
+    sort(choice: number): Readonly<Employee[]> {
+        switch (choice) {
+            case 1:
+                employees.sort((e1, e2) => e1.id - e2.id)
+                break;
+
+            case 2:
+                employees.sort((e1, e2) => e1.name.toLocaleLowerCase().localeCompare(e2.name.toLocaleLowerCase()))
+                break;
+
+            case 3:
+                employees.sort((e1, e2) => e1.totalSalary - e2.totalSalary)
+                break;
+
+            default:
+                employees.sort((e1, e2) => e1.id - e2.id)
+                break;
+        }
+        return [...employees]
+    }
+}
+
+//user interface
+const anilEmployee = new Employee(1, 'anil', 1000, 2000, 3000)
+anilEmployee.calculateSalary()
+
+const sunilEmployee = new Employee(2, 'sunil', 1500, 2500, 3500)
+sunilEmployee.calculateSalary()
+
+const manager: Manager<Employee> = new EmployeeManager()
+try {
+    console.log(manager.add(anilEmployee) ? 'added' : 'could not add')
+} catch (error: any) {
+    console.log(error.message);
+}
+try {
+    console.log(manager.add(sunilEmployee) ? 'added' : 'could not add')
+} catch (error: any) {
+    console.log(error.message);
+}
+
+const all = manager.getAll()
+if (all.length > 0)
+    all.forEach(e => console.log(`${e.name} has got salary of ${e.totalSalary}`))
+
+const allSorted = manager.sort(2)
+if (allSorted.length > 0)
+    all.forEach(e => console.log(`Name=${e.name}, Id=${e.id}, Total Salary= ${e.totalSalary}`))
+
+
+// employees.push(anilEmployee, sunilEmployee)
+
+// employees.forEach(
+//     function (e) {
+//         e.calculateSalary()
+//         console.log(`${e.name} has got salary of ${e.totalSalary}`);
+//     }
+// )
 /*
 for (let index = 0; index < employees.length; index++) {
     const e = employees[index]
